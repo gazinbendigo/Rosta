918221742c9a036c9dd538a4ae1b9c23beda3b56
holly <garethinbendigo@gmail.com>
2016-02-18 22:40:44 +1100

    Updates

--- server/methods/staffMemberMethods.js
+++ server/methods/staffMemberMethods.js
@@ -5,12 +5,13 @@
 
 Meteor.methods({
     addStaffMember: function(params){
-        check(params, {
-            firstName: String,
-            lastName: String,
-            mobileNumber: Number,
-            order: Number
-        });
+        //check(params, {
+        //    firstName: String,
+        //    lastName: String,
+        //    mobileNumber: Number,
+        //    order: Number
+        //});
+        console.log('hello');
         StaffMembers.insert(params);
     }
 });
